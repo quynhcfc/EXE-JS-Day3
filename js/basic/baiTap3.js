16673d8c5bc46b102ed2ecdc445e034e6747398c
quynhcfc <vanquynh.cfc@gmail.com>
2022-03-22 15:50:12 +0700

    Update js basic baiTap3

--- js/basic/baiTap3.js
+++ js/basic/baiTap3.js
@@ -1,28 +1,31 @@
 function chanLe() {
-  var number1 = document.getElementById("exe3-txt-number1").value;
+  var number1 = document.getElementById("exe3-txt-number1").value * 1;
 
-  var number2 = document.getElementById("exe3-txt-number2").value;
+  var number2 = document.getElementById("exe3-txt-number2").value * 1;
 
-  var number3 = document.getElementById("exe3-txt-number3").value;
+  var number3 = document.getElementById("exe3-txt-number3").value * 1;
+
+  var numberArray = [number1, number2, number3];
+
+  var chan = 0;
+  var le = 0;
 
   var kq1 = document.getElementById("exe3-ket-qua1");
   var kq2 = document.getElementById("exe3-ket-qua2");
-  var kq3 = document.getElementById("exe3-ket-qua3");
 
   if (number1 == "" || number2 == "" || number3 == "") {
     alert("Vui lòng nhập số !!!");
   } else {
-    var a =
-      (number1 * 1) % 2 == 0
-        ? (kq1.innerText = "Chẵn")
-        : (kq1.innerText = "Lẻ");
-    var b =
-      (number2 * 1) % 2 == 0
-        ? (kq2.innerText = "Chẵn")
-        : (kq2.innerText = "Lẻ");
-    var c =
-      (number3 * 1) % 2 == 0
-        ? (kq3.innerText = "Chẵn")
-        : (kq3.innerText = "Lẻ");
+    for (var i = 1; i < numberArray.length; i++) {
+      if (numberArray[i] % 2 == 0) {
+        // console.log("Chan");
+        chan = chan + i;
+      } else {
+        // console.log("Le");
+        le = le + i;
+      }
+    }
+    kq1.innerText = chan + " số chẵn";
+    kq2.innerText = le + " số lẻ";
   }
 }
